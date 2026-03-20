68459e133db3c0611c2453fb96a56dc52a3344fa
GIRT Pipeline <girt@epiphytic.dev>
2026-03-20 08:32:38 -0700

    feat: add ANSI escape code stripping

--- src/ansi.rs
+++ src/ansi.rs
@@ -1,3 +1,60 @@
-pub fn strip_ansi(_input: &str) -> String {
-    todo!()
+pub fn strip_ansi(input: &str) -> String {
+    let mut out = String::with_capacity(input.len());
+    let mut chars = input.chars();
+    while let Some(c) = chars.next() {
+        if c == '\x1b' {
+            if let Some(next) = chars.next() {
+                if next == '[' {
+                    for param in chars.by_ref() {
+                        if param.is_ascii_alphabetic() {
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            out.push(c);
+        }
+    }
+    out
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn strips_simple_color_code() {
+        assert_eq!(strip_ansi("\x1b[31mred\x1b[0m"), "red");
+    }
+
+    #[test]
+    fn strips_bold_and_reset() {
+        assert_eq!(strip_ansi("\x1b[1mbold\x1b[0m"), "bold");
+    }
+
+    #[test]
+    fn preserves_plain_text() {
+        assert_eq!(strip_ansi("hello world"), "hello world");
+    }
+
+    #[test]
+    fn strips_256_color() {
+        assert_eq!(strip_ansi("\x1b[38;5;196mred\x1b[0m"), "red");
+    }
+
+    #[test]
+    fn strips_rgb_color() {
+        assert_eq!(strip_ansi("\x1b[38;2;255;0;0mred\x1b[0m"), "red");
+    }
+
+    #[test]
+    fn strips_cursor_movement() {
+        assert_eq!(strip_ansi("\x1b[2J\x1b[Hstart"), "start");
+    }
+
+    #[test]
+    fn handles_empty_string() {
+        assert_eq!(strip_ansi(""), "");
+    }
 }
